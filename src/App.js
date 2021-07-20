c0432e67db8455673ded36a3978292de90bbe018
thecthebest <asyed5839@gmail.com>
2021-07-20 23:12:20 +0200

    Wrapped exisiting components between <Layout> Component
    All the wrapped components will be passed to <Layout> component this will allow them to be rendered along with the Main Navigation bar. This will ensure that components whose URL path matches will be displayed whereas other wont be.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import { Redirect, Route, Switch } from "react-router-dom";
+import Layout from "./components/layout/Layout";
 import AllQuotes from "./pages/AllQuotes";
 import NewQuote from "./pages/NewQuote";
 import QuoteDetail from "./pages/QuoteDetail";
 function App() {
   return (
-    <Switch>
-      <Route path="/" exact>
-        <Redirect to="/quotes"/>
-      </Route>
-      <Route path="/quotes" exact>
-        <AllQuotes />
-      </Route>
-      <Route path="/quotes/:quotesId">
-        <QuoteDetail />
-      </Route>
-      <Route path="/new-quotes">
-        <NewQuote />
-      </Route>
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
+        <Route path="/quotes" exact>
+          <AllQuotes />
+        </Route>
+        <Route path="/quotes/:quotesId">
+          <QuoteDetail />
+        </Route>
+        <Route path="/new-quotes">
+          <NewQuote />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
