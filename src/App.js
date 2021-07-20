a901960e5776b70758c8dc37034752a290db25e9
thecthebest <asyed5839@gmail.com>
2021-07-20 22:13:44 +0200

    Added another Route to redirect
    When the Application page starts and the path ends with "/" the Redirect move to "/quotes".

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import AllQuotes from "./pages/AllQuotes";
 import NewQuote from "./pages/NewQuote";
 import QuoteDetail from "./pages/QuoteDetail";
 function App() {
   return (
     <Switch>
+      <Route path="/" exact>
+        <Redirect to="/quotes"/>
+      </Route>
       <Route path="/quotes" exact>
         <AllQuotes />
       </Route>
