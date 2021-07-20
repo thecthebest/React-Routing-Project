bb4f0b01213ab8ee1a6abacde1acc01851607ff1
thecthebest <asyed5839@gmail.com>
2021-07-20 23:23:21 +0200

    Added some dummy data
    The dummy data will be used to render some quotes through <QuoteList> component.

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,3 +1,9 @@
+const Dummy_Quotes = [
+    {id: 'q1', author: 'Sam', text: 'Learning React is fun!'},
+    {id: 'q2', author: 'Tom', text: 'Learning Ruby is fun!'},
+    {id: 'q3', author: 'Mario', text: 'Learning JavaScript is fun!'},
+];
+
 function AllQuotes() {
     return (
         <h1>All Quotes Page</h1>
