31749d9f9a3945554b168bcb2bf38df673a1a166
thecthebest <asyed5839@gmail.com>
2021-07-20 21:57:54 +0200

    Added Various Components inside the App Component
    Components have been added to the specific routes that will allow them to display according to the URL.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { Route, Switch } from "react-router-dom";
-
+import AllQuotes from "./pages/AllQuotes";
+import NewQuote from "./pages/NewQuote";
+import QuoteDetail from "./pages/QuoteDetail";
 function App() {
   return (
     <Switch>
       <Route path="/quotes">
-
+        <AllQuotes />
       </Route>
       <Route path="/quotes/:quotesId">
-
+        <QuoteDetail />
       </Route>
       <Route path="/new-quotes">
-
+        <NewQuote />
       </Route>
     </Switch>
   );
