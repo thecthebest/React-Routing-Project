75b31d05f37e58ba2dc867cad1ccd157419d885d
thecthebest <asyed5839@gmail.com>
2021-07-20 22:22:31 +0200

    Extract "quotesId" inside <QuoteDetail>
    Any information after "/quotes/" needs to be extracted and shown from <QuoteDetail> Component, for this useParms has been used which returns an object. The object allows to get information by using "quotesId" as a property.

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,6 +1,13 @@
+import { Fragment } from "react";
+import { useParams } from "react-router-dom";
+
 function QuoteDetail() {
+    const params = useParams();
     return (
-        <h1>Quote Detail Page</h1>
+        <Fragment>
+            <h1>Quote Detail Page</h1>
+            <p>{params.quotesId}</p>
+        </Fragment>
     );
 }
 
