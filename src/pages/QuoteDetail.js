40d34213f74e96a373704917a6b927821d2b3471
thecthebest <asyed5839@gmail.com>
2021-07-21 12:08:20 +0200

    Added Dummy_Quotes
    The Dummy_Quotes is used to render quotes whose "id" matches "quoteId" via ".find()" method.

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,8 +1,15 @@
 import { Fragment } from "react";
 import { Route, useParams } from "react-router-dom";
 import Comments from "../components/comments/Comments";
-
+const Dummy_Quotes = [
+    {id: 'q1', author: 'Sam', text: 'Learning React is fun!'},
+    {id: 'q2', author: 'Tom', text: 'Learning Ruby is fun!'},
+    {id: 'q3', author: 'Mario', text: 'Learning JavaScript is fun!'},
+];
 function QuoteDetail() {
+    const quote = Dummy_Quotes.find((quote) => {
+        return quote.id === params.quotesId;
+    });
     const params = useParams();
     return (
         <Fragment>
