f31c6e87a001ecedafa6a612e4b53487179fa273
thecthebest <asyed5839@gmail.com>
2021-07-21 12:52:35 +0200

    Added programmatic navigation option
    Once pressing on "Add quote" then "useHistory" hook will take the user to the path specified in "history.push('/quotes')".

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,10 +1,14 @@
+import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
 function NewQuote() {
+    // To navigate programmatically
+    const history = useHistory();
     const addQuoteHandler = (quoteData) => {
         console.log(quoteData);
+        history.push('/quotes');
     };
     return (
-        <QuoteForm onAddQuote={addQuoteHandler}/>
+        <QuoteForm onAddQuote={addQuoteHandler} />
     );
 }
 
