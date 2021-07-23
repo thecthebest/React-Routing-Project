bd9b3bd78f85ba20e6cd06712d873b70a8061db0
thecthebest <asyed5839@gmail.com>
2021-07-23 10:20:01 +0200

    Added a nested <Link> for showing "Load Comments"
    <Link> is shown when the path in URL is "/quotes/id" and when the <Link> is clicked then <Comments> component is shown.

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { Route, useParams } from "react-router-dom";
+import { Link, Route, useParams } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 const Dummy_Quotes = [
@@ -18,6 +18,12 @@ function QuoteDetail() {
     return (
         <Fragment>
             <HighlightedQuote text={quote.text} author={quote.author} />
+            <Route path={`/quotes/${params.quotesId}`} exact>
+            <div className="centered">
+                <Link className="btn--flat" to={`/quotes/${params.quotesId}/comments`} exact>Load Comments</Link>
+            </div>
+            </Route>
+            
             <Route path={`/quotes/${params.quotesId}/comments`}>
                 <Comments />
             </Route>
