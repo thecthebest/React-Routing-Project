c61c41c1e5bc292776d354f0ecead523d39ac24d
thecthebest <asyed5839@gmail.com>
2021-07-23 17:18:20 +0200

    Added funtionality to fetch data from Google Firebase
    Custom "useHttp" hook was used and "getAllquotes" function is passed as an argument. "useHttp" then returns various constants such as "sendRequest", "status" and so on. In Addition, "useEffect" is used to send request to Google Firebase RealTime database and when the "status" is received it is then checked and according to various states different action are taken.

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,13 +1,36 @@
+import { useEffect } from "react";
 import QuoteList from "../components/quotes/QuoteList";
-const Dummy_Quotes = [
-    {id: 'q1', author: 'Sam', text: 'Learning React is fun!'},
-    {id: 'q2', author: 'Tom', text: 'Learning Ruby is fun!'},
-    {id: 'q3', author: 'Mario', text: 'Learning JavaScript is fun!'},
-];
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import useHttp from "../hooks/use-http";
+import { getAllQuotes } from "../lib/api";
+import NoQuotesFound from "../components/quotes/NoQuotesFound";
 
 function AllQuotes() {
+    const { sendRequest, status, data: loadedQuotes, error } = useHttp(getAllQuotes, true);
+    useEffect(() => {
+        sendRequest();
+    }, [sendRequest]);
+    if (status === 'pending') {
+        return (
+            <div className='centered'>
+                <LoadingSpinner />
+            </div>
+        );
+    }
+    if (error) {
+        return (
+            <div className='centered focused'>
+                {error}
+            </div>
+        );
+    }
+    if (status === 'completed' && (!loadedQuotes || loadedQuotes.length === 0)) {
+        return (
+            <NoQuotesFound />
+        );
+    }
     return (
-        <QuoteList quotes={Dummy_Quotes}/>
+        <QuoteList quotes={loadedQuotes} />
     );
 }
 
