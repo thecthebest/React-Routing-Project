fde83e606c88a67e5cbdb5ac674f0296f142d5e7
thecthebest <asyed5839@gmail.com>
2021-07-23 16:45:17 +0200

    Added "useHttp" hook and "addQuote" function
    "addQuote" function is passed to "useHttp" which returns an object a method and a property. "sendRequest" method takes the data recived by "addQuoteHandler". In Addition, there is "useState" hook to take the user back to "/quotes" when "status ==== completed".

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,11 +1,19 @@
+import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
+import useHttp from "../hooks/use-http";
+import { addQuote } from "../lib/api";
 function NewQuote() {
     // To navigate programmatically
     const history = useHistory();
+    const {sendRequest, status} = useHttp(addQuote);
+    useEffect(() => {
+        if (status === 'completed') {
+            history.push('/quotes');
+        } 
+    }, [status, history]);
     const addQuoteHandler = (quoteData) => {
-        console.log(quoteData);
-        history.push('/quotes');
+        sendRequest(quoteData);
     };
     return (
         <QuoteForm onAddQuote={addQuoteHandler} />
